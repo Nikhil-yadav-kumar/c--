7be10da0f1ed7f4e54e5af408d8b7fbbae37cad3
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-08-28 11:11:44 +0530

    1st

--- 7.cpp
+++ 7.cpp
@@ -5,7 +5,7 @@ int main(){
     cout<<"enter the numbers:";
     cin>>n;
     for(int i=0;i<n;i++){
-        for(int j=n-1;j>0;j--){
+        for(int j=i;j<n;j++){
             cout<<" ";
         }
         for(int k=1;k<=i;k++){
