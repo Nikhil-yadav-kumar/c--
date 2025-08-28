cf023fd7a3c53aea4ad207cdc9b57da0dddbcdfc
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2025-08-28 11:16:04 +0530

    2nd

--- 8.cpp
+++ 8.cpp
@@ -1,5 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
+    int day;
+    cout << "Enter a number to display the days of the week: ";
+    cin >> day;
+
+    switch(day) {
+        case 1:
+            cout << "Sunday" << endl;
+            break;
+        case 2:
+            cout << "Monday" << endl;
+            break;
+        case 3:
+            cout << "Tuesday" << endl;
+            break;
+        case 4:
+            cout << "Wednesday" << endl;
+            break;
+        case 5:
+            cout << "Thursday" << endl;
+            break;
+        case 6:
+            cout << "Friday" << endl;
+            break;
+        case 7:
+            cout << "Saturday" << endl;
+            break;
+        default:
+            cout << "Invalid" << endl;
+    }
+
     return 0;
 }
